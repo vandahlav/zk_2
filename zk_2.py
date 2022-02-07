c00a80d1f8fad13c486fe1448d8ae697a65e0f51
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2022-02-07 22:26:12 +0100

    ošetření výjimek

--- zk_2.py
+++ zk_2.py
@@ -1,3 +1,4 @@
+from calendar import month_name
 from math import sqrt
 
 #loading variables
@@ -12,20 +13,31 @@ x = list [0]
 y = list [1]
 z = list [2]
 
-"""
-x=[1,1]
-y=[2,8]
-z=[-6,2]"""
-
 #calculation of the center of the circle
-def calculation (X, Y, Z):
+def getCircleCenterAndRadius (X, Y, Z):
     (x1, y1), (x2, y2), (x3, y3) = X, Y, Z
+    if y1 == y2 == y3:
+        print("Points are in one line. Program cannot calculate coordinates of the circle.")
+        quit()
+    if x1 == x2 == x3:
+        print("Points are in one line. Program cannot calculate coordinates of the circle.")
+        quit()           
+
     a = x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2
     b = (x1 ** 2 + y1 ** 2) * (y3 - y2) + (x2 ** 2 + y2 ** 2) * (y1 - y3) + (x3 ** 2 + y3 ** 2) * (y2 - y1)
     c = (x1 ** 2 + y1 ** 2) * (x2 - x3) + (x2 ** 2 + y2 ** 2) * (x3 - x1) + (x3 ** 2 + y3 ** 2) * (x1 - x2)
-    m = (-b / a / 2)
-    n = (-c / a / 2)
-    r = sqrt((x1-m)**2+(y1-n)**2)
-    return print(f"The center of circle is in point with coordinates [{m}, {n}], the radius of the circle is: {r}.")
+    try:
+        m = (-b / a / 2)
+        n = (-c / a / 2)
+        if x1 == m:
+            print("Cannot process, program will end.")
+            quit()
+        if y1 == n:
+            print("Cannot process, program will end.")
+            quit()
+        r = sqrt((x1-m)**2+(y1-n)**2)
+    except ZeroDivisionError:
+        print("Cannot divide with zero.")
+    return print(f"The center of circle is in point with coordinates [{m}, {n}], the radius of the circle is: {r:.3f}.")
 
-calculation (x,y,z)
+getCircleCenterAndRadius (x,y,z)
